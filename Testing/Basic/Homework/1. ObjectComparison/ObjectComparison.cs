dbf10bc956727e28aab3104d6cbea149bc01e924
hold192v2 <agrigorev664@gmail.com>
2025-10-30 00:13:34 +0500

    Домашняя работа по Тестированию. Изменения в ObjectComparison.cs и NumberValidatorTests.cs

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using NUnit.Framework.Legacy;
+using FluentAssertions;
 
 namespace HomeExercise.Tasks.ObjectComparison;
 public class ObjectComparison
@@ -10,22 +11,35 @@ public class ObjectComparison
     public void CheckCurrentTsar()
     {
         var actualTsar = TsarRegistry.GetCurrentTsar();
-
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
             new Person("Vasili III of Russia", 28, 170, 60, null));
 
         // Перепишите код на использование Fluent Assertions.
-        ClassicAssert.AreEqual(actualTsar.Name, expectedTsar.Name);
-        ClassicAssert.AreEqual(actualTsar.Age, expectedTsar.Age);
-        ClassicAssert.AreEqual(actualTsar.Height, expectedTsar.Height);
-        ClassicAssert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-        ClassicAssert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+        expectedTsar.Should().BeEquivalentTo(actualTsar, options => 
+             options.Excluding(x => x.Id)
+                    .Excluding(x => x.Parent.Id));
+        
+        // ClassicAssert.AreEqual(actualTsar.Name, expectedTsar.Name);
+        // ClassicAssert.AreEqual (actualTsar.Age, expectedTsar.Age);
+        // ClassicAssert.AreEqual(actualTsar.Height, expectedTsar.Height);
+        // ClassicAssert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
+        //
+        // ClassicAssert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
+        // ClassicAssert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
+        // ClassicAssert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
+        // ClassicAssert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
     }
-
+    
+    //Основные проблемы, которые я выделелил:
+    //1. Расширяемость.
+    //В сравнении с Fluent Assertions, который автоматически проверяет поля у класса, этот вариант использует ручную проверку каждого поля,
+    //что будет ломать тест после добавления новых полей в классе.
+    //В таком случае необходимо будет переписывать тест, что займет больше времени, относительно решения выше. 
+    //2. Отсутствие отчетности.
+    //При падении теста будет непонятно, где находится различие, т.к. будет выведено только True или False
+    //3. Неуниверсальность.
+    //Такое решение не подходит для написания тестов на большое количество объектов, т.к. необходимо писать метод AreEqual для каждого из объектов.
+    //4. Читаемость:)
     [Test]
     [Description("Альтернативное решение. Какие у него недостатки?")]
     public void CheckCurrentTsar_WithCustomEquality()
--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿
+using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
@@ -7,25 +8,54 @@ namespace HomeExercise.Tasks.NumberValidator;
 [TestFixture]
 public class NumberValidatorTests
 {
-    [Test]
-    public void Test()
+    [TestCase(-1, 2, true, TestName = "Negative precision.")]
+    [TestCase(1, -2, true,  TestName = "Negative scale.")]
+    [TestCase(4, 5, true,  TestName = "Precision is less than scale.")]
+    [TestCase(5, 5, true,  TestName = "Scale is equal to precision.")]
+    [TestCase(0, 0, true,  TestName = "Precision and scale are equal to zero.")]
+    public void Constructor_ShouldThrowArgumentException_ForInvalidArguments(int precision, int scale, bool allowSigned)
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+        Action act = () => new NumberValidator(precision, scale, allowSigned);
+        act.Should().Throw<ArgumentException>(); 
     }
+    
+    [TestCase(4, 3, true, TestName = "Precision is greater than scale.")]
+    [TestCase(3, 0, true, TestName = "Scale is equal to zero.")]
+    [TestCase(5, 0, false, TestName = "Scale is equal to zero with changed sign.")]
+    public void Constructor_DoNotThrowArgumentException_ForValidArguments(int precision, int scale, bool allowSigned)
+    {
+        Action act = () => new NumberValidator(precision, scale, allowSigned);
+        act.Should().NotThrow(); 
+    }
+    
+    [TestCase(17, 3, true,"0", TestName = "Integer positive value with scale > 0.")]
+    [TestCase(10, 3, true,"999999999.0", TestName = "Fractional positive value with scale > 0 and length = precision.")]
+    [TestCase(17, 3, true,"0.0", TestName = "Fractional positive value with scale > 0.")]
+    [TestCase(4, 3, true,"0.000", TestName = "Fractional positive value with fractional part length = scale.")]
+    [TestCase(2, 0, false,"-0", TestName = "Fractional negative value with scale = 0 and precision = value length.")]
+    [TestCase(3, 1, false,"-0", TestName = "Fractional negative value.")]
+    [TestCase(17, 3, false,"-0.000", TestName = "Fractional negative value and fractional part length = scale.")]
+    [TestCase(4, 2, true,"+0.00", TestName = "Fractional positive value with length with '+' = precision.")] 
+    public void IsValidNumber_ShouldBeTrue_ForValidArguments(int precision, int scale, bool allowSigned, string value)
+    {
+        var numberValidator = new NumberValidator(precision, scale, allowSigned);
+        numberValidator.IsValidNumber(value).Should().BeTrue();
+    }
+    
+    [TestCase(17, 2, true,"-0", TestName = "Negative integer value with onlyPositive flag.")]
+    [TestCase(3, 2, true,"00.00", TestName = "Value length != precision.")]
+    [TestCase(3, 2, true,"-0.00", TestName = "Negative fractional value with onlyPositive flag and fractional part length = scale.")]
+    [TestCase(17, 2, true,"0.000", TestName = "Scale is greater than fractional part length.")]
+    [TestCase(3, 2, true,"+1.23", TestName = "Fractional positive value with length without '+' = precision.")]
+    [TestCase(3, 2, true,"-1.23", TestName = "Fractional negative value with length without '-' = precision.")]
+    [TestCase(3, 2, true,"a.sd", TestName = "Value is not number")]
+    [TestCase(4, 2, false,null, TestName = "Value is null.")]
+    [TestCase(4, 2, false,"", TestName = "Value is empty string.")]
+    public void IsValidNumber_ShouldBeFalse_ForInvalidArguments(int precision, int scale, bool allowSigned, string value)
+    {
+        var numberValidator = new NumberValidator(precision, scale, allowSigned);
+        numberValidator.IsValidNumber(value).Should().BeFalse();
+    }
+    
+
 }
